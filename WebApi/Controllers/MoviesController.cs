dce30f5e57abbd6597924214098b8cb95040c9c1
Jambukesh <jkesh360@gmail.com>
2024-01-12 16:49:48 +0200

    day6

---
+++ WebApi/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : Controller
+    {
+        private static readonly List<Movie> movies = new List<Movie>()
+        {
+            new() {Id=1,Mname="jaihind",Gen="Action"},
+            new() {Id=2,Mname="Endiran",Gen="Si-Fi"},
+            new() {Id=3,Mname="The nun",Gen="Horror"}
+
+        };
+        [HttpGet(Name = "GetMovies")]
+        public IEnumerable<Movie> Get()
+        {
+            return movies;
+        }
+    }
+}
---
+++ WebApi/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Models
+{
+    public class Movie
+    {
+        public int  Id { get; set; }
+        public string  Mname { get; set; }
+
+        public string Gen { get; set; }
+
+
+    }
+}
